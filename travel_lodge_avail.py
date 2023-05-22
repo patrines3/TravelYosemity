42e09bb480c0f7ee95654bb4e93e645f05108e6c
Patricia <patrines3@gmail.com>
2023-05-22 12:16:54 -0700

    remove classified data from file, changed parameters

--- travel_lodge_avail.py
+++ travel_lodge_avail.py
@@ -1,6 +1,6 @@
 # Import libraries
-#import pandas as pd
 import time
+import datetime
 import os
 import logging
 from selenium import webdriver
@@ -8,17 +8,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import StaleElementReferenceException
-
-#import token
+from token_twilio import key_twilio, to_phone, from_phone, alt_phone
 from twilio.rest import Client
 
+# Set Twilio to send sms messages
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 account_sid = "AC76e55aab103ecc85c59dcb3b7189444e"
-auth_token = '6003a2aa36d6aa34502e933033a4b386' #os.environ["TWILIO_AUTH_TOKEN"]
+auth_token = key_twilio #os.environ["TWILIO_AUTH_TOKEN"]
+
 
 ## Setup chrome options
 chrome_options = Options()
@@ -30,52 +30,67 @@ logging.getLogger('WDM').setLevel(logging.NOTSET)
 os.environ['WDM_LOG'] = 'False'
 
 # Creating a driver objet to manage browser 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Defining the url we want to scrape
 url_pages = {"Travel Lodge": "https://reservations.ahlsmsworld.com/Yosemite/Plan-Your-Trip/Accommodations",} 
 
+# Get data from the webpage of interest
 for page in url_pages:    
     # Ask the driver to direct us to the url
     driver.get(url_pages[page])
-    print("Processing" , url_pages[page] )
-    # Get table view
+    print("Processing" , url_pages[page], datetime.datetime.now() )
+
     time.sleep(2)
     
     select = Select(driver.find_element(By.ID,'box-widget_ProductSelection'))
-    # select by visible text
+
+    # select Lodge of interest by visible text
     select.select_by_visible_text('Yosemite Valley Lodge')
     time.sleep(1)
 
+    # select Number of rooms by visible text
     select = Select(driver.find_element(By.ID,'box-widget_UnitCount'))
-    
-    select.select_by_visible_text('3')
+    select.select_by_visible_text('1')
 
-    select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[0]'))
+    # select Number of children in the room by visible text
+    select = Select(driver.find_element(By.ID,'box-widget_Children'))
     select.select_by_visible_text('1')
-    select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[1]'))
-    select.select_by_visible_text('1')
-    select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[2]'))
-    select.select_by_visible_text('1')
-    
+    # The following lines are for setting multiple rooms
+    #select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[0]'))
+    #select.select_by_visible_text('1')
+    #select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[1]'))
+    #select.select_by_visible_text('1')
+    #select = Select(driver.find_element(By.ID,'box-widget_MultiUnitCount_Children[2]'))
+    #select.select_by_visible_text('1')
+    time.sleep(1)
+
+    # open calendar of check-in dates
     driver.find_element(By.ID, "box-widget_ArrivalDate").click()
 
+    # select month of check-in to evaluate
+    chosen_month = 'Jun'
     select = Select(driver.find_element(By.CLASS_NAME,'ui-datepicker-month'))
-    select.select_by_visible_text('Jun')
-
+    select.select_by_visible_text(chosen_month)
+    
+    time.sleep(1)
     days_selector = driver.find_elements(By.CSS_SELECTOR, 'tr')
     
-    avail = driver.find_elements(By.CSS_SELECTOR, '.ui-datepickerAvail-significant:not(.ui-datepicker-unselectable), .ui-datepickerAvail-limited:not(.ui-datepicker-unselectable)')
-
+    avail = driver.find_elements(By.CSS_SELECTOR, '.ui-datepickerAvail-significant:not(.ui-datepicker-unselectable), .ui-datepickerAvail-limited:not(.ui-datepicker-unselectable)') #
+    
     days = []
-
+    avail_days = []
     try:
         for day in avail:
             if day.text != '':
                 days.append(int(day.text))
     except StaleElementReferenceException as Exception:
-         print('no hay dias disponibles')      
-
+        #pass
+        print('no hay dias disponibles')      
+        #    columns = driver.find_elements(By.CSS_SELECTOR, 'td')
+        #    print(columns[0].class)
+        #    #for i,j in enumerate(columns):
+        #    #    print(columns[i].text)
     finally:
         counter = 1
         for i,day in enumerate(days):
@@ -83,13 +98,16 @@ for page in url_pages:
                 continue
             if days[i] == days[i-1] + 1:
                 counter += 1
-            else: 
+            else:
+                # restart counter
                 counter = 1
 
             if counter == 4:
                 break
-
-        if counter > 4:
-            client = Client(account_sid, auth_token)
-            message = client.messages.create( body="Hay disponibilidad!",from_="+18882985392",to="+15629556375")
     
+        if counter == 4: #  & (days[0] == 8)
+            client  = Client(account_sid, auth_token)
+            print('Hay disponibilidad ')
+            print(' ,'.join(str(day) for day in days))
+            message = client.messages.create( body='Hay disponibilidad en ' + chosen_month + ' ' + ' ,'.join(str(day) for day in days),from_=from_phone,to=to_phone)
+            message = client.messages.create( body='Hay disponibilidad en ' + chosen_month + ' ' + ' ,'.join(str(day) for day in days),from_=from_phone,to=alt_phone)
